711f3c471a96a76becda888e6757b99dcd2ef3cb
Brett Buddin <brett@buddin.us>
2015-08-29 19:05:34 -0400

    Triplet and Dotted tests.

--- note_test.go
+++ note_test.go
@@ -12,17 +12,20 @@ func TestNoteDuration(test *testing.T) {
 		bpm      int
 		expected time.Duration
 	}{
-		{D1, D4, 60, time.Duration(4) * time.Second},
-		{D4, D4, 60, time.Duration(1) * time.Second},
-		{D8, D8, 60, time.Duration(250) * time.Millisecond},
-		{D1, D8, 60, time.Duration(2) * time.Second},
+		{D1, D4, 60, 4 * time.Second},
+		{D4, D4, 60, 1 * time.Second},
+		{D8, D8, 60, 250 * time.Millisecond},
+		{D1, D8, 60, 2 * time.Second},
+		{D1, D8, 60, 2 * time.Second},
+		{Triplet(D4), D4, 60, (1 * time.Second / 3.0)},
+		{Dotted(D4), D4, 60, 1500 * time.Millisecond},
 	}
 
 	for i, t := range data {
 		actual := NewNote(C, 0, Natural, t.duration).Time(t.unit, t.bpm)
 
 		if actual != t.expected {
-			test.Errorf("index=%d actual=%d expected=%s", i, actual, t.expected)
+			test.Errorf("index=%d actual=%s expected=%s", i, actual, t.expected)
 		}
 	}
 }
