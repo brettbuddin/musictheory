e9b65b85283136b9c4e7d9283b8f328ff316a9c8
Brett Buddin <brett@buddin.us>
2017-01-28 10:37:37 -0500

    Move named interval series into another package.

--- scale.go
+++ scale.go
@@ -1,58 +1,5 @@
 package musictheory
 
-// Scales
-var (
-	ChromaticIntervals       []Interval
-	MajorIntervals           []Interval
-	MinorIntervals           []Interval
-	MajorPentatonicIntervals []Interval
-	MinorPentatonicIntervals []Interval
-	IonianIntervals          []Interval
-	DorianIntervals          []Interval
-	PhrygianIntervals        []Interval
-	AeolianIntervals         []Interval
-	LydianIntervals          []Interval
-	MixolydianIntervals      []Interval
-	LocrianIntervals         []Interval
-)
-
-func init() {
-	P1 := Perfect(1)
-	P4 := Perfect(4)
-	P5 := Perfect(5)
-
-	M2 := Major(2)
-	M3 := Major(3)
-	M6 := Major(6)
-	M7 := Major(7)
-
-	m2 := Minor(2)
-	m3 := Minor(3)
-	m6 := Minor(6)
-	m7 := Minor(7)
-
-	A4 := Augmented(4)
-	d5 := Diminished(5)
-
-	ChromaticIntervals = []Interval{P1, m2, M2, m3, M3, P4, A4, P5, m6, M6, m7, M7}
-
-	IonianIntervals = []Interval{P1, M2, M3, P4, P5, M6, M7}
-	MajorIntervals = IonianIntervals
-
-	DorianIntervals = []Interval{P1, M2, m3, P4, P5, M6, m7}
-	PhrygianIntervals = []Interval{P1, m2, m3, P4, P5, m6, m7}
-	LydianIntervals = []Interval{P1, M2, M3, A4, P5, M6, M7}
-	MixolydianIntervals = []Interval{P1, M2, M3, P4, P5, M6, m7}
-
-	AeolianIntervals = []Interval{P1, M2, m3, P4, P5, m6, m7}
-	MinorIntervals = AeolianIntervals
-
-	MajorPentatonicIntervals = []Interval{P1, M2, M3, P5, M6}
-	MinorPentatonicIntervals = []Interval{P1, m3, P4, P5, m7}
-
-	LocrianIntervals = []Interval{P1, m2, m3, P4, d5, m6, m7}
-}
-
 // Scale is a series of Transposers
 type Scale []Transposer
 
--- scale_test.go
+++ scale_test.go
@@ -1,13 +1,16 @@
-package musictheory
+package musictheory_test
 
 import (
 	"reflect"
 	"testing"
+
+	mt "github.com/brettbuddin/musictheory"
+	"github.com/brettbuddin/musictheory/intervals"
 )
 
 type scaleTest struct {
-	root      Pitch
-	intervals []Interval
+	root      mt.Pitch
+	intervals []mt.Interval
 	octaves   int
 	expected  []string
 }
@@ -16,28 +19,28 @@ var scaleTests []scaleTest
 
 func init() {
 	scaleTests = []scaleTest{
-		{NewPitch(C, Natural, 4), ChromaticIntervals, 1, []string{"C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4"}},
-		{NewPitch(C, Natural, 4), IonianIntervals, 1, []string{"C4", "D4", "E4", "F4", "G4", "A4", "B4"}},
-		{NewPitch(C, Natural, 4), DorianIntervals, 1, []string{"C4", "D4", "Eb4", "F4", "G4", "A4", "Bb4"}},
-		{NewPitch(C, Natural, 4), PhrygianIntervals, 1, []string{"C4", "Db4", "Eb4", "F4", "G4", "Ab4", "Bb4"}},
-		{NewPitch(C, Natural, 4), LydianIntervals, 1, []string{"C4", "D4", "E4", "Gb4", "G4", "A4", "B4"}},
-		{NewPitch(C, Natural, 4), MixolydianIntervals, 1, []string{"C4", "D4", "E4", "F4", "G4", "A4", "Bb4"}},
-		{NewPitch(C, Natural, 4), AeolianIntervals, 1, []string{"C4", "D4", "Eb4", "F4", "G4", "Ab4", "Bb4"}},
-		{NewPitch(C, Natural, 4), LocrianIntervals, 1, []string{"C4", "Db4", "Eb4", "F4", "Gb4", "Ab4", "Bb4"}},
-		{NewPitch(C, Natural, 4), MajorIntervals, 2, []string{"C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5"}},
-		{NewPitch(C, Natural, 4), MinorIntervals, 1, []string{"C4", "D4", "Eb4", "F4", "G4", "Ab4", "Bb4"}},
-		{NewPitch(E, Natural, 4), MinorPentatonicIntervals, 1, []string{"E4", "G4", "A4", "B4", "D5"}},
-		{NewPitch(E, Flat, 4), MajorPentatonicIntervals, 1, []string{"Eb4", "F4", "G4", "Bb4", "C5"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Chromatic, 1, []string{"C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Ionian, 1, []string{"C4", "D4", "E4", "F4", "G4", "A4", "B4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Dorian, 1, []string{"C4", "D4", "Eb4", "F4", "G4", "A4", "Bb4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Phrygian, 1, []string{"C4", "Db4", "Eb4", "F4", "G4", "Ab4", "Bb4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Lydian, 1, []string{"C4", "D4", "E4", "Gb4", "G4", "A4", "B4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Mixolydian, 1, []string{"C4", "D4", "E4", "F4", "G4", "A4", "Bb4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Aeolian, 1, []string{"C4", "D4", "Eb4", "F4", "G4", "Ab4", "Bb4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Locrian, 1, []string{"C4", "Db4", "Eb4", "F4", "Gb4", "Ab4", "Bb4"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Major, 2, []string{"C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5"}},
+		{mt.NewPitch(mt.C, mt.Natural, 4), intervals.Minor, 1, []string{"C4", "D4", "Eb4", "F4", "G4", "Ab4", "Bb4"}},
+		{mt.NewPitch(mt.E, mt.Natural, 4), intervals.MinorPentatonic, 1, []string{"E4", "G4", "A4", "B4", "D5"}},
+		{mt.NewPitch(mt.E, mt.Flat, 4), intervals.MajorPentatonic, 1, []string{"Eb4", "F4", "G4", "Bb4", "C5"}},
 	}
 }
 
 func TestScales(test *testing.T) {
 	for i, t := range scaleTests {
-		scale := NewScale(t.root, t.intervals, t.octaves)
+		scale := mt.NewScale(t.root, t.intervals, t.octaves)
 		actual := []string{}
 
 		for _, p := range scale {
-			actual = append(actual, p.(Pitch).Name(DescNames))
+			actual = append(actual, p.(mt.Pitch).Name(mt.DescNames))
 		}
 
 		if !reflect.DeepEqual(actual, t.expected) {
