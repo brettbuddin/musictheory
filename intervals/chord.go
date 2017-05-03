17c08e121a42daa6fa348d6a728506f119b51e01
Brett Buddin <brett@buddin.us>
2017-05-03 18:22:52 -0400

    Additional chords and a fix for the aug7 chord.

--- intervals/chord.go
+++ intervals/chord.go
@@ -3,20 +3,22 @@ package intervals
 import mt "buddin.us/musictheory"
 
 var (
-	MajorTriad,
-	MajorSeventh,
-	MajorSixth,
-	MinorTriad,
-	MinorSeventh,
-	MinorMajorSeventh,
-	MinorSixth,
-	DiminishedTriad,
-	DiminishedMajorSeventh,
-	DiminishedSeventh,
-	AugmentedTriad,
 	AugmentedMajorSeventh,
 	AugmentedSeventh,
-	AugmentedSixth []mt.Interval
+	AugmentedSixth,
+	AugmentedTriad,
+	DiminishedMajorSeventh,
+	DiminishedSeventh,
+	DiminishedTriad,
+	DominantSeventh,
+	HalfDiminishedSeventh,
+	MajorSeventh,
+	MajorSixth,
+	MajorTriad,
+	MinorMajorSeventh,
+	MinorSeventh,
+	MinorSixth,
+	MinorTriad []mt.Interval
 )
 
 func init() {
@@ -33,13 +35,13 @@ func init() {
 
 	A4 := mt.Augmented(4)
 	A5 := mt.Augmented(5)
-	A7 := mt.Augmented(7)
 	d5 := mt.Diminished(5)
 	d7 := mt.Diminished(7)
 
 	MajorTriad = []mt.Interval{P1, M3, P5}
 	MajorSeventh = []mt.Interval{P1, M3, P5, M7}
 	MajorSixth = []mt.Interval{P1, M3, P5, M6}
+	DominantSeventh = []mt.Interval{P1, M3, P5, m7}
 
 	MinorTriad = []mt.Interval{P1, m3, P5}
 	MinorMajorSeventh = []mt.Interval{P1, m3, P5, M7}
@@ -49,9 +51,10 @@ func init() {
 	DiminishedTriad = []mt.Interval{P1, m3, d5}
 	DiminishedMajorSeventh = []mt.Interval{P1, m3, d5, M7}
 	DiminishedSeventh = []mt.Interval{P1, m3, d5, d7}
+	HalfDiminishedSeventh = []mt.Interval{P1, m3, d5, m7}
 
 	AugmentedTriad = []mt.Interval{P1, M3, A5}
 	AugmentedMajorSeventh = []mt.Interval{P1, M3, A5, M7}
-	AugmentedSeventh = []mt.Interval{P1, M3, A5, A7}
+	AugmentedSeventh = []mt.Interval{P1, M3, A5, m7}
 	AugmentedSixth = []mt.Interval{P1, A4, m6}
 }
